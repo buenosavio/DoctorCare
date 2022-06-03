2c409129e0daa57d6a268bef05ea73ad9eb139ba
Sávio de Souza  Bueno <savio.souza@dbccompany.com.br>
2022-06-03 15:09:05 -0300

    feat: aula 1 e 2 ok

---
+++ Origin/main.js
@@ -0,0 +1,15 @@
+function onScroll() {
+  if (scrollY > 0) {
+    navigate.classList.add('scroll')
+  } else {
+    navigate.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
